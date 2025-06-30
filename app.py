3a8532bdbd98a6fed71146ce4d51f6a2977adb37
Danilo Araujo <devdanilobr@gmail.com>
2025-06-30 18:44:40 -0300

    version 0.7.4

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template  # type: ignore
+import os
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-  
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
+    port = int(os.environ.get("PORT", 5000))  # usa a porta do ambiente ou 5000 como padrão
     app.run(host="0.0.0.0", port=port, debug=True)
