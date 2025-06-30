9cc09d69d1161cfae8a1f849db4edd072fefca26
Danilo Araujo <devdanilobr@gmail.com>
2025-06-30 20:23:07 -0300

    LETS GO

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # usa a porta do ambiente ou 5000 como padrão
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    app.run()
